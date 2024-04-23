2f09d4d03748d0e8ad84c2dc1cee8db90a2af704
Max Ruiz <76543428+Maxruiz710@users.noreply.github.com>
2024-04-23 15:19:28 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import Home from './pages/Home';
 import Settings from './pages/Settings';
 import BackendDemo from './pages/BackendDemo';
 import EditProfile from './pages/EditProfile';
+import TrendsAnalysis from './pages/TrendsAnalysis';
+import EnterData from './pages/EnterData';
+import ChangeData from './pages/ChangeData';
+import SetGoals from './pages/SetGoals';
 import firebaseConfig from './backend/firebaseConfig';
 import { initializeApp } from 'firebase/app';
 
@@ -21,6 +25,10 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/editprofile" element={<EditProfile />} />
+        <Route path="/trendsanalysis" element={<TrendsAnalysis />} />
+        <Route path="/enterdata" element={<EnterData />} />
+        <Route path="/changedata" element={<ChangeData />} />
+        <Route path="/setgoals" element={<SetGoals />} />
         <Route path="/" element={<BackendDemo />} />
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
